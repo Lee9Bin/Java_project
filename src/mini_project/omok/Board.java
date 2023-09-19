85f9d43a2474c940b4206d069112f1ab9a5bbe57
Lee9Bin <imagen33@daum.net>
2023-09-19 09:26:30 +0900

    오목 게임 만들기

---
+++ src/mini_project/omok/Board.java
@@ -0,0 +1,88 @@
+package mini_project.omok;
+
+import java.util.Scanner;
+
+public class Board {
+	int size;
+	String[][] map;
+
+	Board(int size) {
+		this.size = size;
+		map = new String[size][size];
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				map[row][col] = ".";
+			}
+		}
+	}
+
+	public void print() {
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				if (col == 0) {
+					System.out.printf("%2d", row);
+				}
+				System.out.print(" " + map[row][col]);
+			}
+			System.out.println();
+		}
+		for (int i = 65; i < 65 + size; i++) {
+			if (i == 65) {
+				System.out.print("   ");
+			}
+			System.out.print((char) i + " ");
+		}
+		System.out.println();
+	}
+
+	public void set(Player player) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(player.name+"> ");
+		String[] xy = scanner.nextLine().split(" ");
+
+		int x = Integer.parseInt(xy[1]);
+		int y = ((xy[0].toUpperCase()).charAt(0)) - 65;
+		if (x<0 || y <0 || x>=size || y >=size) {
+			System.out.println("좌표가 범위를 벗어났습니다.");
+			System.out.println("다시 입력해주세요");
+			set(player);
+			return; 
+		}
+		map[x][y] = player.stone;
+	}
+
+	public boolean check(Player player) {
+		int[] dx = { -1, -1, -1, 0, 1, 1, 1, 0 };
+		int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 };
+		String nowStone = player.stone;
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				if (nowStone.equals(map[x][y])) {
+					for (int i = 0; i < 8; i++) {
+						int cnt = 1;
+						int nextx = dx[i] + x;
+						int nexty = dy[i] + y;
+						for (int k = 0; k < 4; k++) {
+
+							if (nextx >= 0 && nexty >= 0 && nextx < size && nexty < size) {
+								if (nowStone.equals(map[nextx][nexty])) {
+									cnt++;
+								}
+							}
+							nextx += dx[i];
+							nexty += dy[i];
+						}
+						if (cnt == 5) {
+							
+							System.out.println("게임이 종료됩니다.");
+							System.out.println("오목이 완성됐습니다.");
+							System.out.println(player.name + "님이 승리하셨습니다.");
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+}
---
+++ src/mini_project/omok/Omok.java
@@ -0,0 +1,29 @@
+package mini_project.omok;
+
+public class Omok {
+	public static void main(String[] args) {
+		Player user = new Player("사용자", "O");
+		Player computer = new Player("컴퓨터", "X");
+		Board board = new Board(19);
+		play(board, user, computer);
+	}
+
+	private static void play(Board board, Player user, Player computer) {
+
+		while (true) {
+			board.print();
+			board.set(user);
+			if (!board.check(user)) {
+				board.print();
+				break;
+			}
+			board.print();
+			board.set(computer);
+			if (!board.check(computer)) {
+				board.print();
+				break;
+			}
+		}
+
+	}
+}
